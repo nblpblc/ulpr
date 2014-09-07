f200f5b24003c99a7aff76c800de7a5ad9bb2066
Sergey Vasiliev <brighterr@gmail.com>
2014-09-07 16:46:30 +0400

    In progress.

--- ch02_02/src/main/java/weatherenrich/WeatherEnrich.java
+++ ch02_02/src/main/java/weatherenrich/WeatherEnrich.java
@@ -1,5 +1,48 @@
 package weatherenrich;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+
 public class WeatherEnrich {
+
+	public static final String RAW_STREAM = "calc_events";
+
+	public static final void main(String[] args) {
+		ConsumerIterator<byte[], byte[]> it = null;
+		ConsumerConnector consumer = kafka.consumer.Consumer
+				.createJavaConsumerConnector(createConfig());
+		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+		topicCountMap.put(RAW_STREAM, new Integer(1));
+		Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer
+				.createMessageStreams(topicCountMap);
+		KafkaStream<byte[], byte[]> stream = consumerMap.get(RAW_STREAM).get(0);
+		while (true) {
+			it = stream.iterator();
+			while (it.hasNext()) {
+				processEvent(new String(it.next().message()));
+			}
+		}
+	}
+
+	private static void processEvent(String raw) {
+		System.out.println(raw);
+	}
+
+	private static ConsumerConfig createConfig() {
+		Properties props = new Properties();
+		props.put("zookeeper.connect", "172.22.13.43:2181");
+		props.put("group.id", "group1");
+		props.put("zookeeper.session.timeout.ms", "10000");
+		props.put("zookeeper.sync.time.ms", "200");
+		props.put("auto.commit.interval.ms", "1000");
+		return new ConsumerConfig(props);
+	}
 
 }
---
+++ ch02_02/src/main/java/weatherenrich/events/Event.java
@@ -0,0 +1,86 @@
+package weatherenrich.events;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.TimeZone;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jackson.JacksonUtils;
+
+public abstract class Event {
+
+	public Subject subject;
+	public String verb;
+	public Context context;
+
+	protected static final ObjectMapper MAPPER = JacksonUtils.newMapper();
+	private static final String STREAM = "calc_events";
+
+	public Event(String verb) {
+		this.subject = new Subject(getHostname());
+		this.verb = verb;
+		this.context = new Context(getTimestamp());
+	}
+
+	public static Producer<String, String> createProducer(String brokers) {
+		Properties props = new Properties();
+		props.put("metadata.broker.list", brokers);
+		props.put("serializer.class", "kafka.serializer.StringEncoder");
+		props.put("request.required.acks", "1");
+		ProducerConfig config = new ProducerConfig(props);
+		return new Producer<String, String>(config);
+	}
+
+	public void sendTo(Producer<String, String> producer) {
+		String key = this.subject.hostname;
+		String message = this.asJson();
+		KeyedMessage<String, String> data = new KeyedMessage<>(STREAM, key, message);
+		producer.send(data);
+	}
+
+	protected String getTimestamp() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:MM:ssZ");
+		sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+		return sdf.format(new Date());
+	}
+
+	private String getHostname() {
+		try {
+			return InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException uhe) {
+			return "unknown";
+		}
+	}
+
+	public String asJson() {
+		try {
+			return MAPPER.writeValueAsString(this);
+		} catch (JsonProcessingException jpe) {
+			throw new RuntimeException("Problem converting event to JSON", jpe);
+		}
+	}
+
+	public static class Subject {
+		public final String hostname;
+
+		public Subject(String hostname) {
+			this.hostname = hostname;
+		}
+	}
+
+	public static class Context {
+		public final String timestamp;
+
+		public Context(String timestamp) {
+			this.timestamp = timestamp;
+		}
+	}
+}
